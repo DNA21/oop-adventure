e4bdd33134d195f4f21d00e2b2ebdcaeb974b769
Daniel Galuardi <dan.galuardi@gmail.com>
2023-08-01 17:06:13 -0400

    fist commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using OOPAdventure;
+
+Text.LoadLanguage(new English());
+
+Console.WriteLine(Text.Language.ChooseYourName);
+
+var name= Console.ReadLine();
+
+if (name == String.Empty)
+    name = Text.Language.DefaultName;
+
+var player = new Player(name);
+
+Console.WriteLine(Text.Language.Welcome, player.Name);
